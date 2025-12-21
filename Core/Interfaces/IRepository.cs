caf6cc2feb65a81074eb4f3e4d1ca6ea926f54ba
Vangjel Dhima <vdhima20@epoka.edu.al>
2025-12-21 20:13:59 +0100

    IRepository added

---
+++ Core/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<IList<T>> GetAllAsync();
+
+    Task<T?> GetByIdAsync(int id);
+
+    void Add(T entity);
+
+    void Edit(T entity);
+
+    void Delete(T entity);
+    
+    bool Exists(int id);
+}
