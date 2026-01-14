0822d9cd4792a6d3bdd15b8b2cd417e4e8fc52cd
ened-hysi <enedhysi@gmail.com>
2026-01-14 12:46:50 +0100

    Adding , ennabling CORS configuration

--- API/Program.cs
+++ API/Program.cs
@@ -57,6 +57,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.UseAuthentication();
 app.UseAuthorization();
 
